6612452c6f7651071f3ff59d7c5cf4d7c2e81115
Gava97 <gava97@gmail.com>
2013-09-10 18:41:11 +0200

    Update AudioPolicyCompatClient.h

--- audio/AudioPolicyCompatClient.h
+++ audio/AudioPolicyCompatClient.h
@@ -15,9 +15,13 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_AUDIOPOLICYCLIENTLEGACY_H
-#define ANDROID_AUDIOPOLICYCLIENTLEGACY_H
+#define LOG_TAG "AudioPolicyCompatClient"
+//#define LOG_NDEBUG 0
 
+#include <stdint.h>
+#include <string.h>
+
+#include <hardware/hardware.h>
 #include <system/audio.h>
 #include <system/audio_policy.h>
 #include <hardware/audio_policy.h>
@@ -25,62 +29,146 @@
 #include <hardware_legacy/AudioSystemLegacy.h>
 #include <hardware_legacy/AudioPolicyInterface.h>
 
-/************************************/
-/* FOR BACKWARDS COMPATIBILITY ONLY */
-/************************************/
+#include "AudioPolicyCompatClient.h"
+
 namespace android_audio_legacy {
 
-class AudioPolicyCompatClient : public AudioPolicyClientInterface {
-public:
-    AudioPolicyCompatClient(struct audio_policy_service_ops *serviceOps,
-                            void *service) :
-            mServiceOps(serviceOps) , mService(service) {}
+audio_module_handle_t AudioPolicyCompatClient::loadHwModule(const char *moduleName)
+{
+    return mServiceOps->load_hw_module(mService, moduleName);
+}
 
-    virtual audio_module_handle_t loadHwModule(const char *moduleName);
+audio_io_handle_t AudioPolicyCompatClient::openOutput(audio_module_handle_t module,
+                                                      audio_devices_t *pDevices,
+                                                      uint32_t *pSamplingRate,
+                                                      audio_format_t *pFormat,
+                                                      audio_channel_mask_t *pChannelMask,
+                                                      uint32_t *pLatencyMs,
+                                                      audio_output_flags_t flags)
+{
+    return mServiceOps->open_output_on_module(mService, module, pDevices, pSamplingRate,
+                                              pFormat, pChannelMask, pLatencyMs,
+                                              flags);
+}
 
-    virtual audio_io_handle_t openOutput(audio_module_handle_t module,
-                                         audio_devices_t *pDevices,
-                                         uint32_t *pSamplingRate,
-                                         audio_format_t *pFormat,
-                                         audio_channel_mask_t *pChannelMask,
-                                         uint32_t *pLatencyMs,
-                                         audio_output_flags_t flags);
-    virtual audio_io_handle_t openDuplicateOutput(audio_io_handle_t output1,
-                                                  audio_io_handle_t output2);
-    virtual status_t closeOutput(audio_io_handle_t output);
-    virtual status_t suspendOutput(audio_io_handle_t output);
-    virtual status_t restoreOutput(audio_io_handle_t output);
-    virtual audio_io_handle_t openInput(audio_module_handle_t module,
-                                        audio_devices_t *pDevices,
-                                        uint32_t *pSamplingRate,
-                                        audio_format_t *pFormat,
-                                        audio_channel_mask_t *pChannelMask);
-    virtual status_t closeInput(audio_io_handle_t input);
-    virtual status_t setStreamOutput(AudioSystem::stream_type stream, audio_io_handle_t output);
-    virtual status_t moveEffects(int session,
-                                 audio_io_handle_t srcOutput,
-                                 audio_io_handle_t dstOutput);
+audio_io_handle_t AudioPolicyCompatClient::openDuplicateOutput(audio_io_handle_t output1,
+                                                          audio_io_handle_t output2)
+{
+    return mServiceOps->open_duplicate_output(mService, output1, output2);
+}
 
-    virtual String8 getParameters(audio_io_handle_t ioHandle, const String8& keys);
-    virtual void setParameters(audio_io_handle_t ioHandle,
-                               const String8& keyValuePairs,
-                               int delayMs = 0);
-    virtual status_t setStreamVolume(AudioSystem::stream_type stream,
-                                     float volume,
-                                     audio_io_handle_t output,
-                                     int delayMs = 0);
-#ifdef QCOM_FM_ENABLED
-    virtual status_t setFmVolume(float volume, int delayMs = 0);
+status_t AudioPolicyCompatClient::closeOutput(audio_io_handle_t output)
+{
+    return mServiceOps->close_output(mService, output);
+}
+
+status_t AudioPolicyCompatClient::suspendOutput(audio_io_handle_t output)
+{
+    return mServiceOps->suspend_output(mService, output);
+}
+
+status_t AudioPolicyCompatClient::restoreOutput(audio_io_handle_t output)
+{
+    return mServiceOps->restore_output(mService, output);
+}
+
+audio_io_handle_t AudioPolicyCompatClient::openInput(audio_module_handle_t module,
+                                                     audio_devices_t *pDevices,
+                                                     uint32_t *pSamplingRate,
+                                                     audio_format_t *pFormat,
+#ifdef STE_AUDIO
+                                                     audio_channel_mask_t *pChannelMask,
+                                                     audio_input_clients *pInputClientId)
+{
+    return mServiceOps->open_input_on_module(mService, module, pDevices,
+                                             pSamplingRate, pFormat, pChannelMask);
+}
+
+status_t AudioPolicyCompatClient::closeInput(audio_io_handle_t input, audio_input_clients *inputClientId)
+{
+    return mServiceOps->close_input(mService, input, inputClientId);
+}
+
+#else
+                                                     audio_channel_mask_t *pChannelMask)
+{
+    return mServiceOps->open_input_on_module(mService, module, pDevices,
+                                             pSamplingRate, pFormat, pChannelMask);
+}
+
+status_t AudioPolicyCompatClient::closeInput(audio_io_handle_t input)
+{
+    return mServiceOps->close_input(mService, input);
+}
 #endif
-    virtual status_t startTone(ToneGenerator::tone_type tone, AudioSystem::stream_type stream);
-    virtual status_t stopTone();
-    virtual status_t setVoiceVolume(float volume, int delayMs = 0);
 
-private:
-    struct audio_policy_service_ops* mServiceOps;
-    void*                            mService;
-};
+status_t AudioPolicyCompatClient::setStreamOutput(AudioSystem::stream_type stream,
+                                             audio_io_handle_t output)
+{
+    return mServiceOps->set_stream_output(mService, (audio_stream_type_t)stream,
+                                          output);
+}
+
+status_t AudioPolicyCompatClient::moveEffects(int session, audio_io_handle_t srcOutput,
+                                               audio_io_handle_t dstOutput)
+{
+    return mServiceOps->move_effects(mService, session, srcOutput, dstOutput);
+}
+
+String8 AudioPolicyCompatClient::getParameters(audio_io_handle_t ioHandle, const String8& keys)
+{
+    char *str;
+    String8 out_str8;
+
+    str = mServiceOps->get_parameters(mService, ioHandle, keys.string());
+    out_str8 = String8(str);
+    free(str);
+
+    return out_str8;
+}
+
+void AudioPolicyCompatClient::setParameters(audio_io_handle_t ioHandle,
+                                            const String8& keyValuePairs,
+                                            int delayMs)
+{
+    mServiceOps->set_parameters(mService, ioHandle, keyValuePairs.string(),
+                           delayMs);
+}
+
+status_t AudioPolicyCompatClient::setStreamVolume(
+                                             AudioSystem::stream_type stream,
+                                             float volume,
+                                             audio_io_handle_t output,
+                                             int delayMs)
+{
+    return mServiceOps->set_stream_volume(mService, (audio_stream_type_t)stream,
+                                          volume, output, delayMs);
+}
+
+#ifdef QCOM_FM_ENABLED
+status_t AudioPolicyCompatClient::setFmVolume(float volume,
+                                              int delayMs)
+{
+    return mServiceOps->set_fm_volume(mService, volume, delayMs);
+}
+#endif
+
+status_t AudioPolicyCompatClient::startTone(ToneGenerator::tone_type tone,
+                                       AudioSystem::stream_type stream)
+{
+    return mServiceOps->start_tone(mService,
+                                   AUDIO_POLICY_TONE_IN_CALL_NOTIFICATION,
+                                   (audio_stream_type_t)stream);
+}
+
+status_t AudioPolicyCompatClient::stopTone()
+{
+    return mServiceOps->stop_tone(mService);
+}
+
+status_t AudioPolicyCompatClient::setVoiceVolume(float volume, int delayMs)
+{
+    return mServiceOps->set_voice_volume(mService, volume, delayMs);
+}
 
 }; // namespace android_audio_legacy
-
-#endif // ANDROID_AUDIOPOLICYCLIENTLEGACY_H
